0968519974e41f9d310085346eb5bd7855c92e54
PJEder12 <jeder@uninorte.edu.co>
2021-04-05 21:52:03 -0500

    Update local files

--- client/src/components/app/baseURL.tsx
+++ client/src/components/app/baseURL.tsx
@@ -1,1 +1,2 @@
-export const baseURL: string = 'http://3.91.114.60:10000/';
+export const baseURL: string = 'http://localhost:10000/';
+// export const baseURL: string = 'http://3.91.114.60:10000/';
