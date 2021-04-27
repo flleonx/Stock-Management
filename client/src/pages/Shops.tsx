20fbdb025bc47eb4afcf6bdeec16ba8fdfa4a49a
PJEder12 <jeder@uninorte.edu.co>
2021-04-27 15:26:25 -0500

    shops try (#17.1)

--- client/src/pages/Shops.tsx
+++ client/src/pages/Shops.tsx
@@ -1,53 +1,53 @@
-import React, { useState, useEffect, useReducer, useRef } from "react";
-import Axios, { AxiosResponse } from "axios";
-import SuccessfulModalDressMaking from "../components/dressmaking/SuccessfulModalDressMaking";
+import React, {useState, useEffect, useReducer, useRef} from 'react';
+import Axios, {AxiosResponse} from 'axios';
+import SuccessfulModalDressMaking from '../components/dressmaking/SuccessfulModalDressMaking';
 // REDUCER
 // import {reducer} from '../components/dressmaking/ReducerDressMaking';
-import "./style/Shops.css";
+import './style/Shops.css';
 // import '../components/dressmaking/style/buttonStyle.css';
-import { baseURL } from "../components/app/baseURL";
-import FilterDropdown from "../components/FilterDropdown";
-import Modal from "../components/Modal";
-import completeImage from "../assets/complete.svg";
-import errorImage from "../assets/error.svg";
-import { StringLiteralLike, updateSourceFile } from "typescript";
+import {baseURL} from '../components/app/baseURL';
+import FilterDropdown from '../components/FilterDropdown';
+import Modal from '../components/Modal';
+import completeImage from '../assets/complete.svg';
+import errorImage from '../assets/error.svg';
+import {StringLiteralLike, updateSourceFile} from 'typescript';
 
 const reducer = (state: any, action: any) => {
-  if (action.type === "SUCCESSFUL_REQUEST") {
+  if (action.type === 'SUCCESSFUL_REQUEST') {
     return {
       ...state,
-      modalContent: "Petición realizada",
+      modalContent: 'Petición realizada',
       imgCheckNumber: 1,
       isModalOpen: true,
     };
   }
-  if (action.type === "WRONG_INPUT") {
+  if (action.type === 'WRONG_INPUT') {
     return {
       ...state,
-      modalContent: "Error: Digite los campos bien",
+      modalContent: 'Error: Digite los campos bien',
       isModalOpen: true,
       imgCheckNumber: 2,
     };
   }
-  if (action.type === "CLOSE_MODAL") {
+  if (action.type === 'CLOSE_MODAL') {
     return {
       ...state,
       isModalOpen: false,
       imgCheckNumber: 0,
-      modalContent: "",
+      modalContent: '',
     };
   }
   return {
     ...state,
     isModalOpen: false,
-    modalContent: "",
+    modalContent: '',
     imgCheckNumber: 0,
   };
 };
 
 const defaultState: any = {
   isModalOpen: false,
-  modalContent: "",
+  modalContent: '',
   imgCheckNumber: 0,
 };
 
@@ -70,22 +70,22 @@ const Shops = () => {
   const [shops, setShops] = useState<IShops[]>([]);
   const [valueReferenceSelect, setValueReferenceSelect] = useState<any>(null);
   const [valueShopSelect, setValueShopSelect] = useState<any>(null);
-  const [amount, setAmount] = useState<string>("");
-  const [selectedReference, setSelectedReference] = useState<string>("");
-  const [selectedShop, setSelectedShop] = useState<string>("");
+  const [amount, setAmount] = useState<string>('');
+  const [selectedReference, setSelectedReference] = useState<string>('');
+  const [selectedShop, setSelectedShop] = useState<string>('');
   const [approvedRequests, setApprovedRequests] = useState<any>([]);
-  const [numberInput, setNumberInput] = useState<string>("");
+  const [numberInput, setNumberInput] = useState<string>('');
   const [infoDeliveryState, setInfoDeliveryState] = useState<any>([]);
   const [infoActualInventory, setInfoActualInventory] = useState<any>([]);
   const [state, dispatch] = useReducer(reducer, defaultState);
   const refContainer: any = useRef(null);
-  const dbReferencesURL: string = baseURL + "api/references";
-  const dbShopsInfoURL: string = baseURL + "api/shopsinformation";
+  const dbReferencesURL: string = baseURL + 'api/references';
+  const dbShopsInfoURL: string = baseURL + 'api/shopsinformation';
   const dbProductsRequestURL: string =
-    baseURL + "api/shopwarehouseproductsrequest";
-  const dbDeliveryState: string = baseURL + "api/deliverystate";
-  const dbActualInventory: string = baseURL + "api/getactualinventory";
-  const dbUpdateReceivedState: string = baseURL + "api/updatereceivedstate";
+    baseURL + 'api/shopwarehouseproductsrequest';
+  const dbDeliveryState: string = baseURL + 'api/deliverystate';
+  const dbActualInventory: string = baseURL + 'api/getactualinventory';
+  const dbUpdateReceivedState: string = baseURL + 'api/updatereceivedstate';
 
   useEffect(() => {
     Axios.get(dbReferencesURL).then((response: AxiosResponse) => {
@@ -115,12 +115,12 @@ const Shops = () => {
       var selectedOption: any = document.querySelector(class1);
       var options: any = document.querySelectorAll(class2);
 
-      selectedOption.addEventListener("click", () => {
-        selectedOption.parentElement.classList.toggle("active");
+      selectedOption.addEventListener('click', () => {
+        selectedOption.parentElement.classList.toggle('active');
       });
 
       options.forEach((option: any) => {
-        option.addEventListener("click", () => {
+        option.addEventListener('click', () => {
           setTimeout(() => {
             selectedOption.innerHTML = option.innerHTML;
             // SET CURRENT REFERENCE VALUE
@@ -129,28 +129,28 @@ const Shops = () => {
               setSelectedReference(option.innerHTML);
               // 1 ==> Shop id
             } else if (setNumber === 1) {
-              setSelectedShop(option.getAttribute("data-id"));
+              setSelectedShop(option.getAttribute('data-id'));
             }
           }, 300);
 
-          selectedOption.parentElement.classList.remove("active");
+          selectedOption.parentElement.classList.remove('active');
         });
       });
     };
   }, []);
 
   const productsRequest = () => {
-    let valueReferenceSelected = "";
-    let valueShopSelected = "";
+    let valueReferenceSelected = '';
+    let valueShopSelected = '';
     let isReferenceExist = 0;
     let isShopExist = 0;
 
     const correctAmount = parseFloat(amount);
     if (valueReferenceSelect === null) {
-      valueReferenceSelected = "";
-    } else if (typeof valueReferenceSelect === "object") {
+      valueReferenceSelected = '';
+    } else if (typeof valueReferenceSelect === 'object') {
       valueReferenceSelected = valueReferenceSelect.referencia.toString();
-    } else if (typeof valueReferenceSelect === "string") {
+    } else if (typeof valueReferenceSelect === 'string') {
       valueReferenceSelected = valueReferenceSelect;
     }
 
@@ -164,10 +164,10 @@ const Shops = () => {
     });
 
     if (valueShopSelect === null) {
-      valueShopSelected = "";
-    } else if (typeof valueShopSelect === "object") {
+      valueShopSelected = '';
+    } else if (typeof valueShopSelect === 'object') {
       valueShopSelected = valueShopSelect.idTienda;
-    } else if (typeof valueShopSelect === "string") {
+    } else if (typeof valueShopSelect === 'string') {
       valueShopSelected = valueShopSelect;
     }
 
@@ -182,8 +182,8 @@ const Shops = () => {
 
     console.log(valueShopSelected);
 
-    let enableInput = valueShopSelected !== "";
-    let enableInput2 = valueReferenceSelected !== "";
+    let enableInput = valueShopSelected !== '';
+    let enableInput2 = valueReferenceSelected !== '';
     if (
       Number.isInteger(correctAmount) &&
       correctAmount > 0 &&
@@ -198,28 +198,28 @@ const Shops = () => {
             idShop: valueShopSelected,
           }).then((response: AxiosResponse): void => {
             console.log(response.data);
-            dispatch({ type: "SUCCESSFUL_REQUEST" });
+            dispatch({type: 'SUCCESSFUL_REQUEST'});
             setValueReferenceSelect(null);
             setValueShopSelect(null);
-            refContainer.current.value = "";
+            refContainer.current.value = '';
           });
         }
       }
     } else {
-      dispatch({ type: "WRONG_INPUT" });
-      refContainer.current.value = "";
+      dispatch({type: 'WRONG_INPUT'});
+      refContainer.current.value = '';
     }
   };
 
   const closeModal = () => {
-    dispatch({ type: "CLOSE_MODAL" });
+    dispatch({type: 'CLOSE_MODAL'});
   };
 
   const handlerReceived = (index: number) => {
     Axios.post(dbUpdateReceivedState, infoDeliveryState[index]).then(
       (response: AxiosResponse) => {
-        setInfoDeliveryState(response.data[0])
-        setInfoActualInventory(response.data[1])
+        setInfoDeliveryState(response.data[0]);
+        setInfoActualInventory(response.data[1]);
       }
     );
   };
@@ -289,7 +289,7 @@ const Shops = () => {
                 Cantidad: {item.cantidadTotal}
               </div>
               <div className="productCard__date">
-                Fecha: {item.timestamp.replace("T", " ").slice(0, 16)}
+                Fecha: {item.timestamp.replace('T', ' ').slice(0, 16)}
               </div>
               <div className="productCard__date">
                 Estado: {item.nombre_estado}
@@ -326,7 +326,7 @@ const Shops = () => {
                 Cantidad: {item.cantidadTotal}
               </div>
               <div className="productCard__date">
-                Fecha: {item.timestamp.replace("T", " ").slice(0, 16)}
+                Fecha: {item.timestamp.replace('T', ' ').slice(0, 16)}
               </div>
               <div className="productCard__date">
                 Estado: {item.nombre_estado}
