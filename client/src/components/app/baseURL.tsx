fabab75cfd216bfb8dd34dff9e12af445cec8af1
flleonx <felipeleonn99@gmail.com>
2021-05-15 05:29:13 -0500

    ip fix

--- client/src/components/app/baseURL.tsx
+++ client/src/components/app/baseURL.tsx
@@ -1,1 +1,5 @@
-export const baseURL: string = 'http://localhost:52000/';
+// DEV
+//export const baseURL: string = 'http://localhost:52000/';
+
+// PRODUCTION
+export const baseURL: string = 'http://gestion-inventarios.ddns.net:52000/';
