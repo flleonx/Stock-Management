6ad0e84b46a7cb671db8b808bb989575c3092a0e
flleonx <felipeleonn99@gmail.com>
2021-05-17 21:55:30 -0500

    timestamp warehouse fix

--- server/src/querys/postRequests/postWareHouseReq.ts
+++ server/src/querys/postRequests/postWareHouseReq.ts
@@ -1,17 +1,17 @@
-import routerStatement, {query} from 'express';
-import {MysqlError} from 'mysql';
-import {runInNewContext} from 'node:vm';
+import routerStatement, { query } from "express";
+import { MysqlError } from "mysql";
+import { runInNewContext } from "node:vm";
 
 //PERSONAL MODULES:
-import database from '../../config/dbConfig';
-
+import database from "../../config/dbConfig";
+import timestamp_generator from "../../chunks/timestamp_generator";
 const router = routerStatement.Router();
 
 //SAVE THE INFORMATION OF BODEGA
-router.post('/api/savecloth', (req: any, res: any) => {
+router.post("/api/savecloth", (req: any, res: any) => {
   let newCode: boolean = true;
   // console.log(req.body.newCloth);
-  let queryVerifyCodes = 'SELECT codigo FROM BODEGA_INSUMOS';
+  let queryVerifyCodes = "SELECT codigo FROM BODEGA_INSUMOS";
   database.query(queryVerifyCodes, (err: MysqlError | null, result: any) => {
     if (err) {
       throw err;
@@ -24,60 +24,62 @@ router.post('/api/savecloth', (req: any, res: any) => {
     if (newCode) {
       addNewCode();
     } else {
-      res.end(JSON.stringify('EXISTING_CODE'));
+      res.end(JSON.stringify("EXISTING_CODE"));
     }
   });
 
   const addNewCode = () => {
-    if (req.body.newCloth.type === 'Tela') {
+    if (req.body.newCloth.type === "Tela") {
       const newCloth = {
         codigo: req.body.newCloth.code,
         color: req.body.newCloth.color,
         metros: req.body.newCloth.amount,
         descripcion: req.body.newCloth.description,
         nombre_imagen: req.body.newCloth.img,
+        timestamp: timestamp_generator(),
       };
       console.log(newCloth);
-      let newClothQuery: string = 'INSERT INTO BODEGA_INSUMOS SET ?';
+      let newClothQuery: string = "INSERT INTO BODEGA_INSUMOS SET ?";
       let query = database.query(
         newClothQuery,
         newCloth,
         (err: MysqlError | null, result: any) => {
           if (err) throw err;
-          res.end(JSON.stringify('SUCCESSFUL_ADDING'));
+          res.end(JSON.stringify("SUCCESSFUL_ADDING"));
         }
       );
     }
-    if (req.body.newCloth.type === 'Insumo') {
+    if (req.body.newCloth.type === "Insumo") {
       const newCloth = {
         codigo: req.body.newCloth.code,
         color: req.body.newCloth.color,
         cantidad: req.body.newCloth.amount,
         descripcion: req.body.newCloth.description,
         nombre_imagen: req.body.newCloth.img,
+        timestamp: timestamp_generator(),
       };
       console.log(newCloth);
-      let newClothQuery: string = 'INSERT INTO BODEGA_INSUMOS SET ?';
+      let newClothQuery: string = "INSERT INTO BODEGA_INSUMOS SET ?";
       let query = database.query(
         newClothQuery,
         newCloth,
         (err: MysqlError | null, result: any) => {
           if (err) throw err;
-          res.end(JSON.stringify('SUCCESSFUL_ADDING'));
+          res.end(JSON.stringify("SUCCESSFUL_ADDING"));
         }
       );
     }
   };
 });
 
-router.post('/api/updatewarehouseinventory', (req, res) => {
+router.post("/api/updatewarehouseinventory", (req, res) => {
   interface IAmountType {
     metros: number;
     cantidad: number;
   }
 
   let queryVerifyField = `SELECT metros, cantidad FROM BODEGA_INSUMOS WHERE codigo = ${req.body.code}`;
-  let field: string = '';
+  let field: string = "";
   let addition: number = 0;
   database.query(
     queryVerifyField,
@@ -87,10 +89,10 @@ router.post('/api/updatewarehouseinventory', (req, res) => {
       }
 
       if (result[0].metros === null) {
-        field = 'cantidad';
+        field = "cantidad";
         addition = result[0].cantidad + parseFloat(req.body.amount);
       } else {
-        field = 'metros';
+        field = "metros";
         addition = result[0].metros + parseFloat(req.body.amount);
       }
 
@@ -101,20 +103,20 @@ router.post('/api/updatewarehouseinventory', (req, res) => {
           if (err) {
             throw err;
           }
-          res.end(JSON.stringify('SUCCESSFUL_UPDATE'));
+          res.end(JSON.stringify("SUCCESSFUL_UPDATE"));
         }
       );
     }
   );
 });
 
-router.post('/api/suppliesrequest', (req, res) => {
+router.post("/api/suppliesrequest", (req, res) => {
   const reference = req.body.referenceSelection;
   const amount = parseInt(req.body.actualAmount);
   var enable = true;
   // RETURN CODES AND AMOUNT OF GOODS IN "CONSUMO_DE_INSUMOS"
   let consumptionQuery =
-    'SELECT codigoycantidad FROM InventoryManagement.MUESTRAS_PRODUCCION WHERE referencia = ?';
+    "SELECT codigoycantidad FROM InventoryManagement.MUESTRAS_PRODUCCION WHERE referencia = ?";
   // ROW DATA PACKET ? TPYE
   database.query(
     consumptionQuery,
@@ -123,7 +125,7 @@ router.post('/api/suppliesrequest', (req, res) => {
       if (err) {
         throw err;
       }
-      let data: string[] = result[0].codigoycantidad.split(',');
+      let data: string[] = result[0].codigoycantidad.split(",");
 
       var suppliesCodes: string[] = []; // CODES OF REQUIRES ITEMS.
       var amountProduction: number[] = []; // AMOUT FOR EACH ITEM 1-->1 RELATION.
@@ -195,17 +197,17 @@ router.post('/api/suppliesrequest', (req, res) => {
                     if (result[0].metros != null) {
                       const diff: number =
                         parseFloat(result[0].metros) - amountProduction[x];
-                      const type: string = 'metros';
+                      const type: string = "metros";
                       triggerF(diff, type);
                     }
                     if (result[0].cantidad != null) {
                       const diff: number =
                         parseFloat(result[0].cantidad) - amountProduction[x];
-                      const type: string = 'cantidad';
+                      const type: string = "cantidad";
                       triggerF(diff, type);
                     }
                     if (x + 1 == suppliesCodes.length) {
-                      res.end(JSON.stringify('SUCCESSFUL_REQUEST'));
+                      res.end(JSON.stringify("SUCCESSFUL_REQUEST"));
                     }
                     x = x + 1;
                   }
@@ -255,8 +257,8 @@ router.post('/api/suppliesrequest', (req, res) => {
   );
 });
 
-router.post('/api/savewarehousedecision', (req, res) => {
-  console.log("EL ID DECISION ES:", req.body.idDecision)
+router.post("/api/savewarehousedecision", (req, res) => {
+  console.log("EL ID DECISION ES:", req.body.idDecision);
   let saveData = {
     referencia: req.body.referencia,
     cantidad: req.body.cantidad,
@@ -266,7 +268,7 @@ router.post('/api/savewarehousedecision', (req, res) => {
   console.log(req.body.timestamp);
   let deleteData = req.body;
   let queryInsertDecision =
-    'INSERT INTO PETICIONES_PROCESADAS_CONFECCION SET ?';
+    "INSERT INTO PETICIONES_PROCESADAS_CONFECCION SET ?";
   database.query(
     queryInsertDecision,
     [saveData],
@@ -282,7 +284,7 @@ router.post('/api/savewarehousedecision', (req, res) => {
       cantidad: req.body.cantidad,
       timestamp: req.body.timestamp,
     };
-    let queryInsertDressMaking = 'INSERT INTO PROCESO_CONFECCION SET ?';
+    let queryInsertDressMaking = "INSERT INTO PROCESO_CONFECCION SET ?";
     database.query(
       queryInsertDressMaking,
       [saveDataDressMaking],
@@ -299,16 +301,16 @@ router.post('/api/savewarehousedecision', (req, res) => {
     if (err) {
       throw err;
     }
-    res.end(JSON.stringify('SUCCESSFUL_SAVING'));
+    res.end(JSON.stringify("SUCCESSFUL_SAVING"));
   });
 });
 
-router.post('/api/temporal', (req, res) => {
+router.post("/api/temporal", (req, res) => {
   let enable = true;
   const reference = req.body.referenceSelection;
   const amount = parseInt(req.body.actualAmount);
   let consumptionQuery =
-    'SELECT codigoycantidad FROM InventoryManagement.MUESTRAS_PRODUCCION WHERE referencia = ?';
+    "SELECT codigoycantidad FROM InventoryManagement.MUESTRAS_PRODUCCION WHERE referencia = ?";
   database.query(
     consumptionQuery,
     [reference],
@@ -316,7 +318,7 @@ router.post('/api/temporal', (req, res) => {
       if (err) {
         throw err;
       }
-      let data: string[] = result[0].codigoycantidad.split(',');
+      let data: string[] = result[0].codigoycantidad.split(",");
 
       var suppliesCodes: string[] = []; // CODES OF REQUIRES ITEMS.
       var amountProduction: number[] = []; // AMOUT FOR EACH ITEM 1-->1 RELATION.
